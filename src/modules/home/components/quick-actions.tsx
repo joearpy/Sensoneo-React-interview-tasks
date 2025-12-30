e6ff6135c8664e3e33b1d2736e6c06720a66cbfd
Arpad Jo <joearpy87@gmail.com>
2025-12-30 10:51:58 +0100

    Viel all products button functionality

--- src/modules/home/components/quick-actions.tsx
+++ src/modules/home/components/quick-actions.tsx
@@ -8,8 +8,11 @@ import { Button } from "../../../components/button";
 import { Plus } from "lucide-react";
 import { Separator } from "../../../components/separator";
 import { AddNewProductModal } from "./add-new-product-modal";
+import { useNavigate } from "react-router";
 
 export function QuickActions() {
+  const navigate = useNavigate();
+
   return (
     <Card className="mt-8">
       <CardHeader>
@@ -17,7 +20,7 @@ export function QuickActions() {
         <Separator className="my-2" />
       </CardHeader>
       <CardContent className="flex gap-3">
-        <Button variant="outline">
+        <Button variant="outline" onClick={() => navigate("/products")}>
           <Plus className="mr-2" />
           View all products
         </Button>
